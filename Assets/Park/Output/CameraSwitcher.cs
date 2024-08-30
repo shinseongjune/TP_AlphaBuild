9f8a252a4646d69f14747870504c96b5f19f5ffc
Hoz94 <zoh9401@gmail.com>
2024-08-30 13:42:30 +0900

    카메라 오류 수정

--- Assets/Park/Output/CameraSwitcher.cs
+++ Assets/Park/Output/CameraSwitcher.cs
@@ -14,12 +14,14 @@ public class CameraSwitcher : GimmickOutput
     {
         vCam.Priority = 10;
         freeLookCam.Priority = 9;
+        isDone = true;
     }
 
     public void buttonFreeLook()
     {
         freeLookCam.Priority = 10;
         vCam.Priority = 9;
+        isDone = true;
     }
 
     public override void Act()
